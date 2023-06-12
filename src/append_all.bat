824fcb417b3e362742b9394107816dadc2c6c904
EwanWallace <erw@wallace2.com>
2023-06-12 20:51:15 +0100

    appends all months from nov 22 - apr 23

---
+++ src/append_all.bat
@@ -0,0 +1,10 @@
+call ..\venv\Scripts\activate
+
+python pipe.py -m nov -y 22 -a
+python pipe.py -m dec -y 22 -a
+python pipe.py -m jan -y 23 -a
+python pipe.py -m feb -y 23 -a
+python pipe.py -m mar -y 23 -a
+python pipe.py -m apr -y 23 -a
+
+pause
