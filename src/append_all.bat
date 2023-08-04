af9a9f59fffd26008d5a4786cbf13995979f5092
EwanWallace <erw@wallace2.com>
2023-08-04 12:17:50 +0100

    up to JUL 23

--- src/append_all.bat
+++ src/append_all.bat
@@ -6,5 +6,8 @@ python pipe.py -m jan -y 23 -a
 python pipe.py -m feb -y 23 -a
 python pipe.py -m mar -y 23 -a
 python pipe.py -m apr -y 23 -a
+python pipe.py -m may -y 23 -a
+python pipe.py -m jun -y 23 -a
+python pipe.py -m jul -y 23 -a
 
 pause
