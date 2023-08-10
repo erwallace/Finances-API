b96264f003ce7de0d3162cce669b9d8b335a89e0
EwanWallace <erw@wallace2.com>
2023-08-10 14:14:45 +0100

    attempt using function variables

--- src/pipe.py
+++ src/pipe.py
@@ -10,7 +10,8 @@ logger = get_logger(__name__)
 
 class pipeline:
 
-    db = SQL()
+    def __init__(self, demo=False):
+        self.db = SQL(demo=demo)
 
     def create_tables_in_db(self) -> None:
         ''' creates all tables in the schema '''
@@ -20,31 +21,31 @@ class pipeline:
         ''' deletes all tables from the schema '''
         self.db.delete_all_tables()
 
-    def append_to_db(self, month_id: str) -> None:
+    def append_to_db(self, month_id: str, demo: bool) -> None:
         ''' iterate through all tables in schema, appending all non-duplicate rows'''
         mz = Monzo(month_id)
-        df_mz, months = mz.preprocess()
+        df_mz, months = mz.preprocess(demo=demo)
         self.db.append_to_db(months, 'months')
         self.db.append_to_db(df_mz, 'spending')
 
         bud = Budget(month_id)
-        df_bud = bud.preprocess()
+        df_bud = bud.preprocess(demo=demo)
         self.db.append_to_db(df_bud, 'budget')
 
         acc = Accounts(month_id)
-        df_acc = acc.preprocess()
+        df_acc = acc.preprocess(demo=demo)
         self.db.append_to_db(df_acc, 'accounts')
 
         inc = Income(month_id)
-        df_inc = inc.preprocess()
+        df_inc = inc.preprocess(demo=demo)
         self.db.append_to_db(df_inc, 'income')
 
         inv = InvestmentVariable(month_id)
-        df_inv = inv.preprocess()
+        df_inv = inv.preprocess(demo=demo)
         self.db.append_to_db(df_inv, 'investments_variable')
 
         inf = InvestmentFixed(month_id)
-        df_inf = inf.preprocess()
+        df_inf = inf.preprocess(demo=demo)
         self.db.append_to_db(df_inf, 'investments_fixed')
 
     @staticmethod
@@ -59,6 +60,7 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--create", action=argparse.BooleanOptionalAction)  # create db tables?
     parser.add_argument("-a", "--append", action=argparse.BooleanOptionalAction)  # append to db?
     parser.add_argument("-d", "--dashboard", action=argparse.BooleanOptionalAction)  # generate dashboard?
+    parser.add_argument("--demo", action=argparse.BooleanOptionalAction)  # use demo database
     args = parser.parse_args()
 
     months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
@@ -68,10 +70,14 @@ if __name__ == '__main__':
     month_id = f'{args.month[:3].upper()} {args.year[-2:]}'
     logging.info(f'month_id is "{month_id}"')
 
+    #if args.demo:
+    #    os.environ['demo'] = 'True'
+
+    pipe = pipeline(demo=args.demo)
     if args.create:
-        pipeline.create_tables_in_db(pipeline)
+        pipe.create_tables_in_db()
     if args.append:
-        pipeline.append_to_db(pipeline, month_id)
+        pipe.append_to_db(month_id, args.demo)
     if args.dashboard:
-        pipeline.generate_dashboard()
+        pipe.generate_dashboard()
 
